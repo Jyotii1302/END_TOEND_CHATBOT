9040107437be2cee41ec73fb9f4b9f5d7bfcf7c8
Jyoti choudhary <jyotiichoudhary1310@gmail.com>
2024-10-06 21:03:44 +0530

    bot.py

--- bot.py
+++ bot.py
@@ -1,13 +1,20 @@
 import json
+import requests
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.pipeline import make_pipeline
 
-# Load training data from the JSON file
-with open('https://raw.githubusercontent.com/Jyotii1302/END_TOEND_CHATBOT/main/training_data.json', 'r') as json_file:
-    training_data = json.load(json_file)
+# Load training data from the JSON file using requests
+url = 'https://raw.githubusercontent.com/Jyotii1302/END_TOEND_CHATBOT/main/training_data.json'
+response = requests.get(url)
+
+# Check if the request was successful
+if response.status_code == 200:
+    training_data = response.json()
+else:
+    raise ValueError(f"Error fetching data: {response.status_code}")
 
 # Check the structure of the loaded data
 if not training_data or len(training_data) == 0:
