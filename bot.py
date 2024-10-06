60d51e57309e7443b109e30cd38974a4592db440
Jyoti choudhary <jyotiichoudhary1310@gmail.com>
2024-10-06 20:49:21 +0530

    bot.py

--- bot.py
+++ bot.py
@@ -1,13 +1,20 @@
 import json
 import pandas as pd
+import requests
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics import accuracy_score
 
-# Load training data from JSON file
-with open('https://raw.githubusercontent.com/jyotii1302/end_toend_chatbot/main/training_data.json', 'r') as json_file:
-    training_data = json.load(json_file)
+# Load training data from JSON file hosted on GitHub
+url = 'https://raw.githubusercontent.com/Jyotii1302/END_TOEND_CHATBOT/main/training_data.json'
+response = requests.get(url)
+
+# Check if the request was successful
+if response.status_code == 200:
+    training_data = response.json()
+else:
+    raise Exception(f"Failed to load data: {response.status_code}")
 
 # Prepare training data
 train_patterns = [entry['instruction (string)'] for entry in training_data]
